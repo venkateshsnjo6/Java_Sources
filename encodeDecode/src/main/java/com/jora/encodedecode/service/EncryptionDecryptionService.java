4fa13d91619cd4c3430143ed34a2cfb0165477d4
VENKATESH R <115777129+venkateshsnjo6@users.noreply.github.com>
2026-06-18 23:35:12 +0530

    Delete encodeDecode directory

--- encodeDecode/src/main/java/com/jora/encodedecode/service/EncryptionDecryptionService.java
+++
@@ -1,8 +0,0 @@
-package com.jora.encodedecode.service;
-
-public interface EncryptionDecryptionService {
-
-	String encrypt(String plainText) throws Exception;
-
-	String decrypt(String encodedText) throws Exception;
-}
